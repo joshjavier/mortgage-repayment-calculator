ea24c1a63ac374c4b3e2efe7f003ada3fb34fa7e
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-08-17 15:12:31 +0800

    chore: install and set up vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare()],
+  plugins: [react(), tailwindcss(), cloudflare(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
