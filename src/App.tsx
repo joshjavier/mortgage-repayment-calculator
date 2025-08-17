f94f791d68f14a4923d489c28732c3ceb7f3a329
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-08-17 17:03:56 +0800

    feat: design theme button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import IconCalculator from './assets/icon-calculator.svg?react'
 import { Button } from './components/ui/button'
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <Button>
+        <IconCalculator />
+        Calculate Repayments
+      </Button>
     </div>
   )
 }
