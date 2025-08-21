4eead7fc137eb98547ef751a423432566d9e7095
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2025-08-21 14:06:20 +0800

    fix: landmark warning

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,9 @@ import { MortgageCalculator } from './feature/mortgage-calculator'
 
 function App() {
   return (
-    <div className="min-h-svh flex-col items-center justify-center sm:flex sm:p-500">
+    <main className="min-h-svh flex-col items-center justify-center sm:flex sm:p-500">
       <MortgageCalculator />
-    </div>
+    </main>
   )
 }
 
